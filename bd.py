4d226befb8f1377f2fcf64935012034de1030862
pavelru <pahaprimorye2015@gmail.com>
2020-04-26 18:26:57 +1000

    top5-product

---
+++ bd.py
@@ -0,0 +1,46 @@
+import pymysql
+import openpyxl
+from pymysql.cursors import DictCursor
+import datetime
+from datetime import datetime, timedelta
+
+
+def get_product():
+    year, month = str(datetime.now())[:7].split("-")
+    month = ["янв", "фев", "мар", "апр", "мая", "июн", "июл", "авг", "сен", "окт", "ноя", "дек"][int(month) - 1]
+    connection = pymysql.connect('37.140.192.116', 'u1001983_mipt', 'MiptHack', 'u1001983_mipthack',
+                                 cursorclass=DictCursor, port=3306)
+    queue = f"""SELECT * FROM COLs WHERE LatestDesiredDeliveryDate like '{'_-' + str(month) + '.-' + str(
+        year)}' or LatestDesiredDeliveryDate like '{'__-' + str(month) + '.-' + str(year)}'"""  # SQL запрос
+    cursor = connection.cursor()
+    cursor.execute(queue)
+    ans = [i["ProductId"] for i in cursor.fetchall()]
+    product_count = dict()
+    for i in set(ans):
+        product_count[i] = ans.count(i)
+    top_5 = [i for i in reversed(sorted(product_count.values()))][:5]
+    res = dict()
+    for i in product_count.keys():
+        if product_count[i] in top_5:
+            connection = pymysql.connect('37.140.192.116', 'u1001983_mipt', 'MiptHack', 'u1001983_mipthack',
+                                         cursorclass=DictCursor, port=3306)
+            queue = f"""SELECT * FROM Supply_orders  WHERE ProductId = '{i}'"""
+            cursor = connection.cursor()
+            cursor.execute(queue)
+            a = cursor.fetchone()
+            res[product_count[i]] = a['Название продукта']
+    return res
+
+
+def get_cols():
+    year, month = str(datetime.now())[:7].split("-")
+    month = ["янв", "фев", "мар", "апр", "мая", "июн", "июл", "авг", "сен", "окт", "ноя", "дек"][int(month) - 1]
+    connection = pymysql.connect('37.140.192.116', 'u1001983_mipt', 'MiptHack', 'u1001983_mipthack', cursorclass=DictCursor, port=3306)
+    queue = f"""SELECT * FROM COLs WHERE LatestDesiredDeliveryDate like '{'_-' + str(month) +'.-' + str(year)}' or LatestDesiredDeliveryDate like '{'__-' + str(month) +'.-' + str(year)}'"""# SQL запрос
+    cursor = connection.cursor()
+    cursor.execute(queue)
+    ans = [i["LatestDesiredDeliveryDate"] for i in cursor.fetchall()]
+    res = dict()
+    for i in set(ans):
+        res[i] = ans.count(i)
+    return res
